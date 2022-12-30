9216e1ce5fcf06011e2e322b5106a2f44803e711
catter1 <63272345+catter1@users.noreply.github.com>
2022-12-30 20:08:57 +0100

    new: migrated /stats to stats cog

---
+++ cogs/stats.py
@@ -0,0 +1,164 @@
+import discord
+import requests
+import typing
+import json
+import socket
+import urllib3
+import time
+import datetime
+from discord import app_commands
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+
+class Stats(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+
+	async def cog_load(self):
+		#self.loop_get_stats.start()
+		print(f' - {self.__cog_name__} cog loaded.')
+
+	async def cog_unload(self):
+		#self.loop_get_stats.stop()
+		print(f' - {self.__cog_name__} cog unloaded.')
+
+	@tasks.loop(hours=6.0)
+	async def loop_get_stats(self):
+		self.stats = await self.get_stats()
+
+	@loop_get_stats.before_loop
+	async def before_change_presence(self):
+		await self.client.wait_until_ready()
+
+	### COOLDOWNS & CHECKS ###
+	def in_bot_channel():
+		def bot_channel(interaction: discord.Interaction):
+			if interaction.channel_id == 871376111857193000 or interaction.channel_id == 923571915879231509:
+				return True
+			if interaction.user.guild_permissions.administrator:
+				return True
+			else:
+				return False
+		return app_commands.check(bot_channel)
+
+	def long_cd(interaction: discord.Interaction) -> typing.Optional[app_commands.Cooldown]:
+		if interaction.user.guild_permissions.administrator:
+			return None
+		return app_commands.Cooldown(1, 35.0)
+
+	### COMMANDS ###
+	
+	@app_commands.command(name="stats", description="Shows stats about Stardust Labs")
+	@in_bot_channel()
+	@app_commands.checks.dynamic_cooldown(long_cd)
+	async def stats(self, interaction: discord.Interaction, member: discord.Member = None):
+		""" /stats [member]"""
+
+		# Individual member stats
+		if member:
+
+			joined = int(time.mktime(member.joined_at.timetuple()))
+			created = int(time.mktime(member.created_at.timetuple()))
+
+			embed = discord.Embed(
+				title=f"{member.display_name}'s Stats",
+				color=self.client.get_user(member.id).color,
+				description=f"<:stardust:1058423314672013382> joined <t:{joined}:R>\n<:discord:1048627498734342206> joined <t:{created}:R>"
+			)
+			
+			embed.add_field(name="Total Messages", value="Coming Soon", inline=False)
+			embed.add_field(name="Most Used Commands", value="Coming Soon")
+			embed.set_thumbnail(url=member.display_avatar.url)
+			embed.set_footer(text=f"{member.name}#{member.discriminator}")
+
+			await interaction.response.send_message(embed=embed)
+
+		# Stardust Labs stats
+		else:
+			stats = self.stats
+			
+			embed = discord.Embed(color=discord.Colour.brand_red())
+			embed.set_author(name=interaction.guild.name, icon_url=interaction.guild.icon)
+			embed.add_field(name='Incendy Version', value=self.version)
+			embed.add_field(name='Member Count', value=interaction.guild.member_count)
+			embed.add_field(name='StardustTV', value="Videos: {:,}".format(stats["videos"] + "\nStreams: {:,}".format(stats["streams"])))
+			#embed.add_field(name='\u200b', value='\u200b')
+			embed.add_field(name='Terralith Downloads', value="{:,}".format(stats["terralith"]))
+			embed.add_field(name='Incendium Downloads', value="{:,}".format(stats["incendium"]))
+			embed.add_field(name='Nullscape Downloads', value="{:,}".format(stats["nullscape"]))
+			embed.add_field(name='Structory Downloads', value="{:,}".format(stats["structory"]))
+			embed.add_field(name='Amplified Nether Downloads', value="{:,}".format(stats["amplified-nether"]))
+			embed.add_field(name='Continents Downloads', value="{:,}".format(stats["continents"]))
+			embed.set_footer(text='<:pmc:1045336243216584744> <:modrinth:1045336248950214706> <:curseforge:1045336245900939274> <:seedfix:917599175259070474>')
+			await interaction.response.send_message(embed=embed)
+
+	async def get_stats(self) -> dict:
+		stats = {}
+
+		with open('resources/stats.json', 'r') as f:
+			stats["streams"] = len(json.load(f)["streams"])
+			stats["videos"] = len(json.load(f)["videos"])
+
+		projects = ["terralith", "incendium", "nullscape", "amplified-nether", "continents", "structory"]
+		for project in projects:
+			stats[project] = 0
+
+		# Curseforge
+		for project in projects:
+			## Thanks! https://python.tutorialink.com/pythons-requests-triggers-cloudflares-security-while-urllib-does-not/
+			answers = socket.getaddrinfo('www.curseforge.com', 443)
+			(family, type, proto, canonname, (address, port)) = answers[0]
+			headers = OrderedDict({
+				'Host': "www.curseforge.com",
+				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+			})
+			s = requests.Session()
+			s.headers = headers
+			urllib3.disable_warnings()
+			response = s.get(f"https://{address}/minecraft/mc-mods/{project}", verify=False)
+
+			soup = BeautifulSoup(response.content, "html.parser")
+			stats[project] += int(soup.find_all(text="Total Downloads")[0].parent.parent.contents[3].text.replace(",", ""))
+		
+		# Modrinth
+		headers = {'User-Agent': 'catter1/Incendy (catter@zenysis.net)'}
+		projects.remove('structory')
+		for project in projects:
+			url = f"https://api.modrinth.com/v2/project/{project}"
+			x = requests.get(url=url, headers=headers)
+			stats[project] += json.loads(x.text)["downloads"]
+
+		# PMC
+		pmc_projects = {
+			"terralith": "terralith-overworld-evolved-100-biomes-caves-and-more/",
+			"incendium": "incendium-nether-expansion",
+			"nullscape": "nullscape",
+			"structory": "structory",
+			"amplified-nether": "amplified-nether-1-18/",
+			"continents": "continents"
+		}
+		for project in pmc_projects.keys():
+			url = f"https://www.planetminecraft.com/data-pack/{pmc_projects[project]}"
+			x = requests.get(url=url, headers=headers)
+			soup = BeautifulSoup(x.text, "html.parser")
+			stats[project] += int(soup.find_all(text=" downloads, ")[0].parent.contents[1].text.replace(",", ""))
+
+		# Seedfix
+		url = "https://seedfix.stardustlabs.net/api/get_downloads/"
+		x = requests.get(url=url, headers=headers)
+		stats["terralith"] += int(x.text)
+
+		return stats
+
+	async def on_app_command_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+		if isinstance(error, app_commands.CommandOnCooldown):
+			if interaction.command.name == "stats":
+				await interaction.response.send_message("Yikes! " + str(error), ephemeral=True)
+		elif isinstance(error, app_commands.CheckFailure):
+			if interaction.command.name == "stats":
+				await interaction.response.send_message("This command can only be used in a bot command channel like <#871376111857193000>.", ephemeral=True)
+
+async def setup(client):
+	await client.add_cog(Stats(client))
