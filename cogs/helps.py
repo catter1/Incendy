8dc9e798e9e9e155fcc92993d6f5b18fcc29f73b
catter1 <63272345+catter1@users.noreply.github.com>
2023-02-05 22:05:01 +0100

    change: adjusted support thread æsthetic

--- cogs/helps.py
+++ cogs/helps.py
@@ -64,10 +64,10 @@ class Helps(commands.Cog):
 	async def on_thread_create(self, thread):
 		if thread.parent_id == 1019651246078038128:
 			embed = discord.Embed(
-				colour=discord.Colour.brand_red(),
+				colour=discord.Colour.brand_green(),
 				description="Thanks for creating a thread! Be patient, and we will answer your question when we are able to. In the meantime...\n\n• Check the FAQ by doing `/faq` to see if your question is already answered.\n• Did you **describe** your issue thoroughly?\n• Are relevant **logs** attached?\n• Is your thread **tagged** appropriately?\n• Ensure you've answered the questions outlined in our **Post Guidelines**.\n\nWhen your question is answered, please close it with `/close`. Thank you!"
 			)
-			embed.set_author(name="Your Support Question", icon_url="https://cdn.discordapp.com/emojis/917599175259070474.png")
+			embed.set_author(name="Your Support Question", icon_url="https://cdn.discordapp.com/emojis/1058423314672013382.png")
 
 			await thread.send(embed=embed)
 
