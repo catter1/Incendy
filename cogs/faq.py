7052e299f611f7478afb69f6e3353e58abdf8ac5
catter1 <63272345+catter1@users.noreply.github.com>
2023-02-23 15:57:52 +0100

    fix: codeblock spacing

--- cogs/faq.py
+++ cogs/faq.py
@@ -522,22 +522,23 @@ class ConfigMenu(discord.ui.Select):
                 This tutorial (graciously provided by <@897998124478521356>) will show you how to adjust continent size with the Continents project.
                 **•** Unzip Continents and open the `Continents/data/minecraft/worldgen/density_function/overworld/` folder.
                 **•** Open `base_continents.json`. You should see this: ```json
-                {
-                    "type": "add",
-                    "argument1":{
-                        "argument": {
-                            "xz_scale": 0.13,
-                            "y_scale": 0.0,
-                            "noise": "minecraft:continentalness",
-                            "shift_x": "minecraft:shift_x",
-                            "shift_y": 0.0,
-                            "shift_z": "minecraft:shift_z",
-                            "type": "minecraft:shifted_noise"
-                        },
-                        "type": "minecraft:flat_cache"
-                    },
-                    "argument2":"continents:continent_bias"
-                }```
+{
+    "type": "add",
+    "argument1":{
+        "argument": {
+            "xz_scale": 0.13,
+            "y_scale": 0.0,
+            "noise": "minecraft:continentalness",
+            "shift_x": "minecraft:shift_x",
+            "shift_y": 0.0,
+            "shift_z": "minecraft:shift_z",
+            "type": "minecraft:shifted_noise"
+        },
+        "type": "minecraft:flat_cache"
+    },
+    "argument2":"continents:continent_bias"
+}
+                ```
                 **•** To increase the continent sizes, lower the `xz_scale` value. Halving = 4x larger continents.
                 **•** To decrease the continent sizes, raise the `xz_scale` value. Doubling = 4x smaller continents.
                 **•** Do not change anything else!
