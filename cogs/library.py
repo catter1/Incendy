1563703a8e5b573397d7b2f11ecc1511c8613ef6
catter1 <63272345+catter1@users.noreply.github.com>
2024-06-13 19:28:58 +0200

    fix: #55

--- cogs/library.py
+++ cogs/library.py
@@ -571,7 +571,7 @@ class AmplifiedLinks(discord.ui.View):
 class SparklesDownloads(discord.ui.View):
 	def __init__(self):
 		super().__init__()
-		self.add_item(discord.ui.Button(label='Resourcepack (1.18.2-1.20.x)', emoji=Constants.Emoji.MODRINTH, url='https://modrinth.com/resourcepack/sparkles', row=0))
+		self.add_item(discord.ui.Button(label='Resourcepack (1.19-1.20.x)', emoji=Constants.Emoji.MODRINTH, url='https://modrinth.com/resourcepack/sparkles', row=0))
 		self.add_item(discord.ui.Button(label='ADDON: Alpha Islands (1.19-1.20.x)', emoji=Constants.Emoji.MODRINTH, url='https://modrinth.com/resourcepack/sparkles-addon-alpha-islands', row=1))
 
 class SparklesLinks(discord.ui.View):
