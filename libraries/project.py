3e67b74bce8f71b25aaf41ff40f86dbf0c2b83c0
catter1 <63272345+catter1@users.noreply.github.com>
2026-09-24 20:53:20 +0200

    chore: update 26.3

--- libraries/project.py
+++ libraries/project.py
@@ -389,7 +389,8 @@ class Project:
 			"26.1": 15933,
 			"26.1.1": 16021,
 			"26.1.2": 16082,
-			"26.2": 16498
+			"26.2": 16498,
+			"26.3": 17045
 		}
 		gameVersions = [version_translations[version] for version in self.mc_versions]
 		# This is [Fabric, Forge, NeoForge, Quilt]
